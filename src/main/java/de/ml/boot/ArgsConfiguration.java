79bf97a6507db4043f02fa21b6643b91cd886c6b
Moritz Löser <moritz.loeser@prodyna.com>
2015-11-10 17:42:14 +0100

    port is now configurable, changed log format

--- src/main/java/de/ml/boot/ArgsConfiguration.java
+++ src/main/java/de/ml/boot/ArgsConfiguration.java
@@ -20,11 +20,16 @@ import org.slf4j.Logger;
 @Singleton
 public class ArgsConfiguration {
 
+    private static final int DEFAULT_PORT = 80;
     private File folder;
+    private List<String> args;
+    @Inject
+    private Logger log;
 
     @Inject
     private ArgsConfiguration(@Parameters List<String> args, Logger log) {
-        checkArgument(args.size() > 0);
+        this.args = args;
+        checkArgument(args.size() > 0, "Missing argument to specify folder.");
         folder = checkFolder(args.get(0));
 
     }
@@ -35,11 +40,27 @@ public class ArgsConfiguration {
         return folder;
     }
 
+    @Produces
+    @Port
+    public Integer getPort(){
+        int port = DEFAULT_PORT;
+        if(args.size()>1){
+            try{
+                port = Integer.parseInt(args.get(1));
+            }catch(NumberFormatException e){
+                log.warn("Second argument (Port) not parsable to integer, given: " + args.get(1));
+            }
+        } else {
+            log.info("Port not specified (2nd argument) using default: " + DEFAULT_PORT);
+        }
+
+        return port;
+    }
 
     private File checkFolder(String string) {
         File file = new File(string);
-        checkArgument(file.isDirectory());
-        checkArgument(file.canRead());
+        checkArgument(file.isDirectory(),"Given argument \"" + file + "\" is not a directory.");
+        checkArgument(file.canRead(),"Can not read from given directory: " + file);
         return file;
     }
 
@@ -47,6 +68,13 @@ public class ArgsConfiguration {
     @Retention(RetentionPolicy.RUNTIME)
     @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
     public static @interface Folder {
+
+    }
+
+    @Qualifier
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+    public static @interface Port {
 
     }
 
--- src/main/java/de/ml/routes/RestRoute.java
+++ src/main/java/de/ml/routes/RestRoute.java
@@ -6,6 +6,7 @@ import org.apache.camel.Endpoint;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 
+import de.ml.boot.ArgsConfiguration.Port;
 import de.ml.endpoints.RestEndpointProvider.RandomImageEndpoint;
 import de.ml.processors.SendFile.SendFileProc;
 
@@ -13,17 +14,19 @@ public class RestRoute extends RouteBuilder {
 
     private Processor sendFile;
     private Endpoint randImageEndpoint;
+    private Integer port;
 
     @Inject
-    private RestRoute(@SendFileProc Processor sendFile, @RandomImageEndpoint Endpoint randImageEndpoint){
+    private RestRoute(@SendFileProc Processor sendFile, @RandomImageEndpoint Endpoint randImageEndpoint,
+                      @Port Integer port) {
         this.sendFile = sendFile;
         this.randImageEndpoint = randImageEndpoint;
-
+        this.port = port;
     }
 
     @Override
     public void configure() throws Exception {
-        restConfiguration().component("restlet").apiContextPath("/").port(8082);
+        restConfiguration().component("restlet").apiContextPath("/").port(port);
         from(randImageEndpoint).process(sendFile);
     }
 
