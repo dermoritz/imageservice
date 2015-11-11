c2f5eec1a091899aab3ee146b06510a6d6ba6a3f
Moritz Löser <moritz.loeser@prodyna.com>
2015-11-11 23:52:07 +0100

    test for one folder

--- src/test/java/de/ml/boot/runMain.java
+++ src/test/java/de/ml/boot/runMain.java
@@ -6,9 +6,7 @@ public class runMain {
 
     @Test
     public void run() {
-        org.jboss.weld.environment.se.StartMain.main(new String[] {"C:\\Users\\moritz\\Downloads\\1\\conny;"
-                                                                 + "C:\\Users\\moritz\\Downloads\\1\\HighResNSFW;"
-                                                                 + "C:\\Users\\moritz\\Downloads\\1\\nsfw_hd"});
+        org.jboss.weld.environment.se.StartMain.main(new String[] {"C:\\Users\\moritz\\Downloads\\1"});
     }
 
 }
