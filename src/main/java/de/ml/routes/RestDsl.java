5d27d152c8a99836ed5530489604fa554abe8551
moritz <moritz.loe@gmail.com>
2017-01-01 15:24:57 +0100

    added a test route using restdsl and swagger api documentation

---
+++ src/main/java/de/ml/routes/RestDsl.java
@@ -0,0 +1,40 @@
+package de.ml.routes;
+
+import de.ml.boot.ArgsConfiguration.Port;
+import de.ml.processors.SendFile;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+
+import javax.inject.Inject;
+
+/**
+ * Created by moritz on 01.01.2017.
+ */
+public class RestDsl extends RouteBuilder {
+
+    private final Integer port;
+    private final Processor sendFile;
+    private static final String DIRECT_NEXT = "direct:next-swagger";
+
+    @Inject
+    public RestDsl(@Port Integer port, @SendFile.SendFileProc Processor sendFile) {
+        this.port = port;
+        this.sendFile = sendFile;
+    }
+
+
+    @Override
+    public void configure() throws Exception {
+        restConfiguration().component("restlet")
+                .host("localhost")
+                .contextPath("/swaggered")
+                .port(port)
+                .apiContextPath("/api-doc")
+                .apiProperty("api.title", "Imageservice REST Api");
+
+        rest("/image")
+                .get("/next").to(DIRECT_NEXT);
+
+        from(DIRECT_NEXT).process(sendFile);
+    }
+}
